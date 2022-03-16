830d61faa0d6b805b58060b66fd461fb6999002c
darkweb455 <ojojoshua18@gmail.com>
2022-03-16 16:09:53 +0800

    added home page

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-import express from 'express';
-import socket from 'socket.io';
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import socket from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
 //setting up App
 const app = express();
 const port = process.env.PORT || 4070;
-const server = app.listen(port,()=>{
-console.log(`server listening to requests on port ${port}`)
+const server = app.listen(port, () => {
+  console.log(`server listening to requests on port ${port}`);
 });
 
 //static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.send("home");
+});
 //socket setup
 const io = socket(server);
-io.on('connection',(socket)=>{
-    console.log('made socket connection', socket.id);
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
 
-    //Handle chat event
-    socket.on('chat',(data)=>{
-        io.sockets.emit('chat', data);
-    });
+  //Handle chat event
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
 
-    socket.on('typing',(data)=>{
-        socket.broadcast.emit('typing',data)
-    })
-})
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
+});
